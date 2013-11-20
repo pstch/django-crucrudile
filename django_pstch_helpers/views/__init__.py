b7626d3043e0b49fc5d6d904c56af2eaa523a064
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-20 14:44:51 +0100

    Fix imports

--- django_pstch_helpers/views/__init__.py
+++ django_pstch_helpers/views/__init__.py
@@ -1,31 +1,31 @@
 from django.shortcuts import render
 from django.contrib import messages
 
-from django.views.generic import View as DjangoView, TemplateView as DjangoTemplateView, DetailView as DjangoDetailView, ListView as DjangoListView
+from django.views.generic import View, TemplateView, ListView, DetailView
 
 from django.views.generic import TemplateView as HaystackSearchView
 
 from .mixins import AuthMixin, ModelInfoMixin, RedirectMixin
 
-class View(DjangoView):
+class View(View):
     pass
 
-class TemplateView(AuthMixin, DjangoTemplateView):
+class TemplateView(AuthMixin, TemplateView):
     pass
 
-class ListView(AuthMixin, ModelInfoMixin, DjangoListView):
+class ListView(AuthMixin, ModelInfoMixin, ListView):
     def get_template_names(self):
         names = super(ListView, self).get_template_names()
         names.append("%s/object_list.html" % self.model._meta.app_label)
         return names
 
-class DetailView(AuthMixin, ModelInfoMixin, DjangoDetailView):
+class DetailView(AuthMixin, ModelInfoMixin, DetailView):
     def get_template_names(self):
         names = super(DetailView, self).get_template_names()
         names.append("%s/object_detail.html" % self.model._meta.app_label)
         return names
 
-class HomeView(AuthMixin, DjangoTemplateView):
+class HomeView(AuthMixin, TemplateView):
     pass
 
 class SearchView(AuthMixin, HaystackSearchView):
