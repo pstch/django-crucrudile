7a43377d10bb35ecb15d9d24ae905663b9eeafd6
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-21 20:19:31 +0100

    view sets

--- django_pstch_helpers/views/sets.py
+++ django_pstch_helpers/views/sets.py
@@ -4,18 +4,18 @@ from .edit import CreateView, DeleteView, UpdateView
 
 PK_ARG = "(?P<pk>\d+)"
 
-EDIT_VIEWS = [ ('create' , CreateView, None),
-               ('update' , UpdateView, [PK_ ARG, ]),
-               ('delete' , DeleteView, [PK_ARG, ]) ]
+EDIT_VIEWS = [('create' , CreateView, None),
+              ('update' , UpdateView, [PK_ARG, ]),
+              ('delete' , DeleteView, [PK_ARG, ])]
 
-LIST_VIEWS = [ ('list' , ListView, None), ]
-DETAIL_VIEWS = [ ('detail' , DetailView, [PK_ARG, ]), ]
+LIST_VIEWS = [('list' , ListView, None),]
+DETAIL_VIEWS = [('detail' , DetailView, [PK_ARG, ]),]
 
-PAGINATED_LIST_VIEWS = [ ('list' , ListView, None), ]
+PAGINATED_LIST_VIEWS = [('list' , ListView, None),]
 
 BASE_VIEWS = LIST_VIEWS + EDIT_VIEWS
 FULL_VIEWS = BASE_VIEWS + DETAIL_VIEWS
 
-SEARCH_VIEWS = [ ('search', SearchView, None), ]
+SEARCH_VIEWS = [('search', SearchView, None),]
 
-MULTIPLE_LIST_VIEWS = [ ('list' , MultipleListView, None), ]
+MULTIPLE_LIST_VIEWS = [('list' , MultipleListView, None),]
