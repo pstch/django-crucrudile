cae3928693a8440bc26f7eaa3e0e407f34c3fcea
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-21 20:05:52 +0100

    fix multiple list views

--- django_pstch_helpers/views/sets.py
+++ django_pstch_helpers/views/sets.py
@@ -4,18 +4,18 @@ from .edit import CreateView, DeleteView, UpdateView
 
 PK_ARG = "(?P<pk>\d+)"
 
-EDIT_VIEWS = [ ('create' , CreateView, None , None),
-               ('update' , UpdateView, [PK_ARG, ], None),
-               ('delete' , DeleteView, [PK_ARG, ], None) ]
+EDIT_VIEWS = [ ('create' , CreateView, None),
+               ('update' , UpdateView, [PK_ ARG, ]),
+               ('delete' , DeleteView, [PK_ARG, ]) ]
 
-LIST_VIEWS = [ ('list' , ListView, None, None), ]
-DETAIL_VIEWS = [ ('detail' , DetailView, [PK_ARG, ], None), ]
+LIST_VIEWS = [ ('list' , ListView, None), ]
+DETAIL_VIEWS = [ ('detail' , DetailView, [PK_ARG, ]), ]
 
-PAGINATED_LIST_VIEWS = [ ('list' , ListView, None, { 'paginate_by' : '50', }), ]
+PAGINATED_LIST_VIEWS = [ ('list' , ListView, None), ]
 
 BASE_VIEWS = LIST_VIEWS + EDIT_VIEWS
 FULL_VIEWS = BASE_VIEWS + DETAIL_VIEWS
 
-SEARCH_VIEWS = [ ('search', SearchView, None, None), ]
+SEARCH_VIEWS = [ ('search', SearchView, None), ]
 
-MULTIPLE_LIST_VIEWS = [ ('list' , MultipleListView, None, None), ]
+MULTIPLE_LIST_VIEWS = [ ('list' , MultipleListView, None), ]
