a9b7c6d6667f9ff845f11e12eec7551cc7d248e6
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-23 04:34:39 +0100

    Add test filtered list view

---
+++ django_pstch_helpers/views/filtered.py
@@ -0,0 +1,26 @@
+from . import ListView
+
+class FilteredListView(ListView):
+    def get_queryset(self):
+        """
+        Return the list of items for this view.
+
+        The return value must be an iterable and may be an instance of
+        `QuerySet` in which case `QuerySet` specific behavior will be enabled.
+        """
+        raise Excepton("filtered list view test")
+        if self.queryset is not None:
+            queryset = self.queryset
+            if isinstance(queryset, QuerySet):
+                queryset = queryset.all()
+        elif self.model is not None:
+            queryset = self.model._default_manager.all()
+        else:
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a QuerySet. Define "
+                "%(cls)s.model, %(cls)s.queryset, or override "
+                "%(cls)s.get_queryset()." % {
+                    'cls': self.__class__.__name__
+                }
+            )
+        return queryset
