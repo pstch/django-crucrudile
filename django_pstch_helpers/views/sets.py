5e336bbef94ed8e459b5ed65093be79c0328fc23
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-23 05:01:14 +0100

    fix import

--- django_pstch_helpers/views/sets.py
+++ django_pstch_helpers/views/sets.py
@@ -5,7 +5,7 @@ from .filtered import FilteredListView
 from .specific import SpecificCreateView
 PK_ARG = "(?P<pk>\d+)"
 FILTER_ARGS = "of-(?P<filter_key>\w+)/(?P<filter_value>\d+)"
-
+SPECIFIC_ARGS = "of-(?P<specific_key>\w+)/(?P<specific_value>\d+)"
 
 
 CREATE_VIEW = { 'create' : ('create',
