2374425858ec68ae405115f9041ec74856a07b07
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-23 17:10:49 +0100

    add context to filtered list view

--- django_pstch_helpers/views/utils.py
+++ django_pstch_helpers/views/utils.py
@@ -21,10 +21,6 @@ def mix_views(*args):
     if len(args) is 1:
         return args[0].copy()
     elif len(args) > 1 :
-        print "## %s ARGUMENTS : %s" % (len(args), args)
-        for arg in args:
-            print "# ARG %s" % arg
-
         current, remaining = mix_intersection(args[0].copy(),
                                            mix_views(*args[1:]))
 
