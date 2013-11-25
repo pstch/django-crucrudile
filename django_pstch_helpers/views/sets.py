defc04338759f06015c4c1c211fdc00dbc9464a1
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-25 05:41:30 +0100

    Revert "test"
    This reverts commit 10fc37c398bc392b60498cd34be7b16c57550a83.

--- django_pstch_helpers/views/sets.py
+++ django_pstch_helpers/views/sets.py
@@ -11,49 +11,49 @@ FILTER_ARGS = "of-(?P<filter_key>\w+)/(?P<filter_value>\d+)"
 SPECIFIC_ARGS = "of-(?P<specific_key>\w+)/(?P<specific_value>\d+)"
 
 
-CREATE_VIEW = { 'create' : ('^create$',
+CREATE_VIEW = { 'create' : ('create$',
                            CreateView,
                            {})}
 
-SPECIFIC_CREATE_VIEW = { 'create-specific' : ('^create/%s$' % SPECIFIC_ARGS,
+SPECIFIC_CREATE_VIEW = { 'create-specific' : ('create/%s$' % SPECIFIC_ARGS,
                          SpecificCreateView,
                          {}),}
 
-UPDATE_VIEW =  { 'update' : ('^update/%s' % PK_ARG,
+UPDATE_VIEW =  { 'update' : ('update/%s' % PK_ARG,
                              UpdateView,
 
                              {}),}
-DELETE_VIEW = { 'delete' : ('^delete/%s' % PK_ARG,
+DELETE_VIEW = { 'delete' : ('delete/%s' % PK_ARG,
                            DeleteView,
                            {}),}
 
-LIST_VIEW = { 'list' : ('^list$',
+LIST_VIEW = { 'list' : ('list$',
                          ListView,
                          {})
            }
 
-MULTIPLE_LIST_VIEW = { 'list' : ('^list$',
+MULTIPLE_LIST_VIEW = { 'list' : ('list$',
                                   MultipleListView,
                                   {})
                     }
-FILTERED_LIST_VIEW = { '^list-filtered' : ('list/%s$' % FILTER_ARGS,
+FILTERED_LIST_VIEW = { 'list-filtered' : ('list/%s$' % FILTER_ARGS,
                                           FilteredListView,
                                   {}),
                     }
 
-DETAIL_VIEW = { 'detail': ('^detail/%s$' % PK_ARG,
+DETAIL_VIEW = { 'detail': ('detail/%s$' % PK_ARG,
                             DetailView,
                             {})
              }
 
-PAGINATED_LIST_VIEW = { 'list' : ('^list$',
+PAGINATED_LIST_VIEW = { 'list' : ('list$',
                                    ListView,
                                    lambda action, view, model: { 'paginate_by' : getattr(model,
                                                                                          'PAGINATE_BY',
                                                                                          50) })
 }
 
-SEARCH_VIEWS = { 'search' : ('^search$',
+SEARCH_VIEWS = { 'search' : ('search',
                              SearchView,
                              {})
 }
