f88836a8b2d192fe83b872b1e507debbfc2a4215
Hugo Geoffroy <pistache@floyd.pstch.net>
2013-11-27 03:36:43 +0100

    Remove FilteredListView, switch to django-filter

--- django_pstch_helpers/views/__init__.py
+++ django_pstch_helpers/views/__init__.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView as HaystackSearchView
 
 from .mixins import AuthMixin, ModelInfoMixin, RedirectMixin
 
-class TemplateView(View, AuthMixin, TemplateView):
+class TemplateView(AuthMixin, TemplateView):
     pass
 
 class ListView(AuthMixin, ModelInfoMixin, ListView):
