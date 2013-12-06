44d6642a378c2f4c78803575ad003c39fbdff3c6
Hugo Geoffroy <pistache@tranquility.pstch.net>
2013-12-06 11:33:15 +0100

    H

--- django_pstch_helpers/views/__init__.py
+++ django_pstch_helpers/views/__init__.py
@@ -11,7 +11,7 @@ from sortable_listview import SortableListView
 class TemplateView(AuthMixin, TemplateView):
     pass
 
-class ListView(AuthMixin, ModelInfoMixin, SortableListView, SelectRelatedMixinc):
+class ListView(AuthMixin, ModelInfoMixin, SortableListView, SelectRelatedMixin):
     def get_template_names(self):
         names = super(ListView, self).get_template_names()
         names.append("%s/object_list.html" % self.model._meta.app_label)
