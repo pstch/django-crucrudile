ac8164f9d15cc59eaaaa50429e95d2f05ccd3d93
Hugo Geoffroy <pistache@tranquility.pstch.net>
2013-12-07 16:50:18 +0100

    OLA

--- django_pstch_helpers/views/search.py
+++ django_pstch_helpers/views/search.py
@@ -54,7 +54,8 @@ class SearchView(FormView):
 
     def form_valid(self, form):
         self.query = form.cleaned_data['q']
+        self.queryset = form.search()
         return super(SearchView, self).form_valid(form)
         
     def get_queryset(self):
-        return self.form.search()
+        return self.queryset
