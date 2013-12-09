2887d78345fdaf7f6739cac6ece29175bca543e9
Hugo Geoffroy <pistache@tranquility.pstch.net>
2013-12-09 03:44:00 +0100

    olé

--- django_pstch_helpers/views/specific.py
+++ django_pstch_helpers/views/specific.py
@@ -7,6 +7,21 @@ from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 from .edit import CreateView
 
+def get_field(model, path):
+    keys = path.split('__')
+    key = keys.pop(0)
+    try:
+        if not keys:
+            return getattr(model, key)
+        else:
+            _field = getattr(model, keys.pop(0))
+            if type(_field) in [ForeignKey, ]:
+                return get_field(_field.rel.to, '__'.join(keys))
+            else:
+                raise ImproperlyConfigured("Wrong relationship path %s : Field %s is not a ForeignKey" % (path, _field))
+    except AttributeError:
+        raise ImproperlyConfigured("Wrong relationship path %s :  Could not find field %s in model %s" % (path, key, model))
+        
 class SpecificCreateView(CreateView):
     initial_keys = []
     def get_initial(self):
@@ -23,11 +38,11 @@ class SpecificCreateView(CreateView):
         key = self.kwargs['specific_key']
         value = self.kwargs['specific_value']
 
-        field = getattr(self.model, key).field
+        field = get_field(self.model, key)
 
         context['initial_field'] = key
 
-        if type(field) in [ForeignKey, ManyToManyField]:
+        if type(field) in [ForeignKey, ]:
 
             model = field.rel.to
 
