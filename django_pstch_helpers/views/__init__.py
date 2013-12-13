bdec27effba98d420dd2b14792a29b0cfb1d40a1
Hugo Geoffroy <pistache@tranquility.pstch.net>
2013-12-13 12:47:48 +0100

    remove search from base views

--- django_pstch_helpers/views/__init__.py
+++ django_pstch_helpers/views/__init__.py
@@ -2,11 +2,10 @@ from .base import *
 from .edit import *
 from .filtered import *
 from .multiple import *
-from .search import *
 
 __all__ = [
     'TemplateView', 'ListView', 'DetailView', 'HomeView',
     'FilteredListView', 'MultipleListView',
     'CreateView','UpdateView','DeleteView',
-    'SearchView'
 ]
+
