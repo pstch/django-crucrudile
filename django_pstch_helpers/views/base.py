676dd77e935e1ef9c3b4de8cc1f5bc1e2ca6fee1
Hugo Geoffroy <pistache@tranquility.pstch.net>
2013-12-14 11:10:48 +0100

    olé

--- django_pstch_helpers/views/base.py
+++ django_pstch_helpers/views/base.py
@@ -11,21 +11,19 @@ class TemplateView(AuthMixin, TemplateView):
     pass
 
 class ListView(AuthMixin, ModelInfoMixin, SortableListView, SelectRelatedMixin):
-    template_name_suffix = 'list'
+    template_name_suffix = '_list'
     def get_template_names(self):
         names = super(ListView, self).get_template_names()
-        suffix = ('_%s' % self.template_name_suffix) \
-                 if self.template_name_suffix else ''
+        suffix = self.template_name_suffix
 
         names.append("%s/object%s.html" % (self.model._meta.app_label, suffix))
         return names
 
 class DetailView(AuthMixin, ModelInfoMixin, DetailView):
-    template_name_suffix = 'detail'
+    template_name_suffix = '_detail'
     def get_template_names(self):
         names = super(DetailView, self).get_template_names()
-        suffix = ('_%s' % self.template_name_suffix) \
-                 if self.template_name_suffix else ''
+        suffix = self.template_name_suffix
 
         names.append("%s/object%s.html" % (self.model._meta.app_label, suffix))
         return names
