d796e1899a606f302fafeaaca1bf5752c081f36e
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-17 04:30:16 +0100

    New base Models adapted to ModelMixins and new CBVs

--- django_pstch_helpers/models.py
+++ django_pstch_helpers/models.py
@@ -6,33 +6,35 @@ from markitup.fields import MarkupField
 
 from .views.sets import BASE_VIEWS
 
-class ModelInfo(models.Model):
-    class Meta:
-        abstract = True
+class ModelInfo():
     @classmethod
-    def get_model_verbose_name(self):
-        return self._meta.verbose_name
+    def _get_objects(self):
+        try:
+            objects = self.objects
+            return objects
+        except AttributeError:
+            raise ImproperlyConfigured("Could not find manager : \"objects\" not present on the current object. Check that ModelInfoMixin is used on a Model.")
+
+    @classmethod
+    def _get_meta(self):
+        try:
+            _meta = self._meta
+            return _meta
+        except AttributeError:
+            raise ImproperlyConfigured("Could not find Meta class : \"_meta\" not present on the current object. Check that ModelInfoMixin is used on a Model.")
+
+    @classmethod
+    def get_verbose_name(self):
+        _meta = self._get_meta()
+        return _meta.verbose_name
     @classmethod
     def get_count(self):
+        objects = self._get_objects()
         return self.objects.count()
     @classmethod
-    def model_name(self):
-        return self._meta.model_name
-    @classmethod
-    def get_label(self):
-        return getattr(self,
-                       'CUSTOM_LABEL',
-                       self._meta.verbose_name)
-    @classmethod
-    def get_plural_label(self):
-        return getattr(self,
-                       'CUSTOM_PLURAL_LABEL',
-                       self._meta.verbose_name_plural)
-    @classmethod
-    def get_help_text(self):
-        return getattr(self,
-                       'HELP_TEXT',
-                       None)
+    def get_model_name(self):
+        _meta = self._get_meta()
+        return _meta.model_name
 
 class AutoPatterns(ModelInfo):
     def get_url_name(self):
@@ -40,58 +42,19 @@ class AutoPatterns(ModelInfo):
     def get_url_prefix(self):
         return None
     def get_views(self):
-        return {}
+        return {} # hey, BASE_VIEWS
     def get_view_args(self):
         return {}
-
-class AutoPatterns(ModelInfo):
-    URL_NAME = None # url name (and default prefix, unless URL_PREFIX is set)
-    URL_PREFIX = None # in django-generic-patterns, if None, URL_NAME will be used
-    URL_VIEWS = BASE_VIEWS # url views
-    URL_VIEW_ARGS = {} # url args
-    URL_NAMESPACES = [] # used to set url namespaces
-    URL_ROOT_NAMESPACE = [] # root NS
+    def get_url_namespaces(self):
+        return []
     @classmethod
+    def _make_url_name(action):
+        return make_url_name(self.get_url_namespaces(),
+                             self.get_url_name(),
+                             action)
     def get_url(self, action, args = None):
-        _namespaces = list(getattr(self, 'URL_ROOT_NAMESPACE', []))
-        for NS in self.URL_NAMESPACES:
-            _namespaces.append(NS)
-        _left = ":".join(_namespaces)
-
-        if self.URL_NAME:
-            _right = "-".join([self.URL_NAME, action])
-        else:
-            _right = action
-
-        _name = "%s:%s" % (_left,
-                           _right )
-
-        try:
-            return reverse(_name, args = args)
-        except:
-            return None
-
-    def get_absolute_url(self):
-        return self.get_url("detail", args = [self.id,])
-    def get_edit_url(self):
-        return self.get_url("update", args = [self.id,])
-    def get_delete_url(self):
-        return self.get_url("delete", args = [self.id,])
-
-    def get_detail_url(self):
-        return self.get_absolute_url()
-    def get_update_url(self):
-        return self.get_edit_url()
-
-    @classmethod
-    def get_list_url(self):
-        return self.get_url("list")
-    @classmethod
-    def get_create_url(self):
-        return self.get_url("create")
-
-    class Meta:
-        abstract = True
+        return reverse(self._make_url_name(action),
+                           args = args)
 
 class UserNamed(ModelInfo):
     name = models.CharField(max_length = 128, verbose_name = "name")
@@ -105,3 +68,56 @@ class UserDescribed(UserNamed):
     description = MarkupField(blank = True, null = True, verbose_name = "description")
     class Meta:
         abstract = True
+
+
+# ANNEX (new branch code)
+
+class DetailableModelMixin(AutoPatterns):
+    def get_detail_url(self):
+        return self.get_url(DetailViewSet.action,
+                            args = [self.id,])
+    def get_absolute_url(self):
+        return self.get_detail_url()
+    def get_views(self):
+        views = super(DetailableModelMixin, self).get_views()
+        contribute_viewset_to_views(views, DetailViewSet)
+        return views
+
+class ListableModelMixin(AutoPatterns):
+    @classmethod
+    def get_list_url(self):
+        return self.get_url(ListViewSet.action)
+        def get_views(self):
+            views = super(ListableModelMixin, self).get_views()
+            contribute_viewset_to_views(views, ListViewSet)
+            return views
+
+class CreatableModelMixin(AutoPatterns):
+    def get_create_url(self):
+        return self.get_url(CreateViewSet.action,
+                            args = [self.id,])
+    def get_views(self):
+        views = super(CreatableModelMixin, self).get_views()
+        contribute_viewset_to_views(views, CreateViewSet)
+        return views
+
+class UpdatableModelMixin(AutoPatterns):
+    def get_update_url(self):
+        return self.get_url(UpdateViewSet.action,
+                            args = [self.id,])
+    def get_edit_url(self):
+        return self.get_update_url()
+        def get_views(self):
+        views = super(UpdatableModelMixin, self).get_views()
+        contribute_viewset_to_views(views, UpdateViewSet)
+        return views
+
+class DeletableModelMixin(AutoPatterns):
+    def get_delete_url(self):
+        return self.get_url(DeleteViewSet.action,
+                            args = [self.id,])
+    def get_views(self):
+        views = super(DeletableModelMixin, self).get_views()
+        contribute_viewset_to_views(views, DeleteViewSet)
+        return views
+
