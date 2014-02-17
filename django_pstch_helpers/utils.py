722a9ba1201b33267ce1b98ce6278e83a7c38cac
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-17 04:28:55 +0100

    Viewset and URL utility functions

--- django_pstch_helpers/utils.py
+++ django_pstch_helpers/utils.py
@@ -1,4 +1,41 @@
+def make_url_name(namespaces, object_url_name, action):
+    """
+    Joins namespaces with an action and optionally an URL name
+
+    Will return "<namespaces>:<object_url_name>-<action>" if object_url_name is not None, otherwise "<namespaces>:<action>" (without the '<>').
+    """
+    return  ":".join(":".join(namespaces),
+                     "-".join[object_url_name, action] if object_url_name else action)
+
+def contribute_viewset_to_views(views, viewset):
+    """
+    Adds the view 3-tuple returned by the viewset to another dict ('views'). If a tuple is already present in views[viewset.action], set the value as list with the two items, and if it's a list, just append the tuple.
+
+    Note: this function does not return any value, as it works directly on the dict given as first argument.
+    """
+    # get action name
+    action = viewset.action
+
+    # get new view tuple
+    new = viewset.get_tuple()
+    # get old dict value
+    current = views.get(action)
+
+    if current:
+        # current not None
+        if isinstance(current, (list)):
+            # list, append
+            views[action].append()
+        else:
+            # singleton, create list
+            views[action] = list(current,
+                                 new)
+    else:
+        # current is None, set new value
+        views[action] = new
+
 def mix_intersection(first, second):
+    # puke before reading for best experience
     if first and second and set(first) & set(second):
         for intersection in list(set(first) & set(second)):
             if isinstance(first[intersection],(list)) and isinstance(second[intersection], (list)):
