01d6b748fd62cfbda0d3c5c3e1f29c9e7245a631
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-18 21:53:08 +0100

    Fix ListView and FilteredListView
    Revert to classic LTR mixins order, and directly use django_sortable_list for mixin

--- django_pstch_helpers/views/list.py
+++ django_pstch_helpers/views/list.py
@@ -1,12 +1,13 @@
 from django.views.generic import ListView
+
 from django_filters.views import FilterView
+from django_sortable_list import SortableListMixin
 
 from .mixins.base import BaseModelMixins
 from .mixins.related import SelectRelatedMixin
-from .mixins.list.sort import SortMixin
 
-class ListView(BaseModelMixins, ListView, SortMixin, SelectRelatedMixin):
+class ListView(BaseModelMixins, SortableListMixin, SelectRelatedMixin, ListView):
     pass
 
-class FilteredListView(BaseModelMixins, FilterView, SortMixin, SelectRelatedMixin):
+class FilteredListView(BaseModelMixins, SortableListMixin, SelectRelatedMixin, FilterView):
     pass
