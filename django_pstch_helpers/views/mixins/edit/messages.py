c44de018f75897336aecfa3e38711d06db490cac
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-20 23:10:55 +0100

    Style and inheritance fixes

--- django_pstch_helpers/views/mixins/edit/messages.py
+++ django_pstch_helpers/views/mixins/edit/messages.py
@@ -2,15 +2,25 @@ from django.views.generic.edit import FormMixin
 
 from django.contrib import messages
 
-class CreateMessageMixin(FormMixin):
+class CreateMessageMixin(object):
     def form_valid(self, form):
-        response = super(CreateView, self).form_valid(form)
-        messages.success(self.request, "Created object %s (of type %s, of id %s) in the database." % (self.object, self.object.__class__.__name__, self.object.id))
+        response = super(CreateMessageMixin, self).form_valid(form)
+        message = "Created object %s (of type %s, of id %s) in the database." \
+                  % (self.object,
+                     self.object.__class__.__name__,
+                     self.object.id)
+        messages.success(self.request,
+                         message)
         return response
 
-class UpdateMessageMixin(FormMixin):
+class UpdateMessageMixin(object):
     def form_valid(self, form):
-        response = super(UpdateView, self).form_valid(form)
-        messages.info(self.request, "Updated object %s (of type %s, of id %s)." % (self.object, self.object.__class__.__name__, self.object.id))
+        response = super(UpdateMessageMixin, self).form_valid(form)
+        message = "Updated object %s (of type %s, of id %s)." \
+                   % (self.object,
+                      self.object.__class__.__name__,
+                      self.object.id)
+        messages.info(self.request,
+                      message)
         return response
 
