3e0abe847ba30fa41dcc8ce8221ad940cc4e4948
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-20 23:03:13 +0100

    Style fixes

--- django_pstch_helpers/views/edit.py
+++ django_pstch_helpers/views/edit.py
@@ -1,13 +1,23 @@
-from django.views.generic import CreateView, UpdateView
+#TODO: Add module docstring
+from django.views.generic import CreateView as DjangoCreateView
+from django.views.generic import UpdateView as DjangoUpdateView
 
 from .mixins.base import BaseModelFormMixins
 from .mixins.edit.messages import CreateMessageMixin, UpdateMessageMixin
 from .mixins.edit.specific import SpecificCreateMixin
-class CreateView(BaseModelFormMixins, CreateMessageMixin, CreateView):
+
+class CreateView(BaseModelFormMixins,
+                 CreateMessageMixin,
+                 CreateView):
     pass
 
-class UpdateView(BaseModelFormMixins, UpdateMessageMixin, UpdateView):
+class UpdateView(BaseModelFormMixins,
+                 UpdateMessageMixin,
+                 DjangoUpdateView):
     pass
 
-class SpecificCreateView(BaseModelFormMixins, CreateMessageMixin, SpecificCreateMixin, CreateView):
+class SpecificCreateView(BaseModelFormMixins,
+                         CreateMessageMixin,
+                         SpecificCreateMixin,
+                         DjangoCreateView):
     pass
