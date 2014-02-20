666c87733a91393a4aa4a1688a0e1a22c19f8047
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-20 23:39:59 +0100

    Cleanup & fix imports

--- django_pstch_helpers/sets/__init__.py
+++ django_pstch_helpers/sets/__init__.py
@@ -3,9 +3,8 @@ from .base import ViewSet
 from .detail import DetailViewSet
 from .list import ListViewSet, FilteredListViewSet
 
-from .edit.create import CreateViewSet, SpecificCreateViewSet
-from .edit.update import UpdateViewSet
-from .edit.delete import DeleteViewSet
+from .edit import CreateViewSet, SpecificCreateViewSet, UpdateViewSet
+from .delete import DeleteViewSet
 
 __all__ = ['ViewSet',
            'DetailViewSet',
