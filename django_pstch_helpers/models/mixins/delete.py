de69e90767ef78cfb49f58e292c7467de739d3e8
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-21 23:23:45 +0100

    Import fix

--- django_pstch_helpers/models/mixins/delete.py
+++ django_pstch_helpers/models/mixins/delete.py
@@ -1,12 +1,13 @@
-from ..utils import contribute_viewset_to_views
+from django_pstch_helpers.utils import contribute_viewset_to_views
 
-from . import AutoPatternsMixin
-from ..sets import DeleteViewSet
+from django_pstch_helpers.sets import DeleteViewSet
+
+from .base import AutoPatternsMixin
 
 class DeletableModelMixin(AutoPatternsMixin):
     def get_delete_url(self):
         return self.get_url(DeleteViewSet.action,
-                            args = [self.id,])
+                            args=[self.id,])
     def get_views(self):
         views = super(DeletableModelMixin, self).get_views()
         contribute_viewset_to_views(views, DeleteViewSet)
