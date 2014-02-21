b23c303bf1d5ffb00a8e84254d0bc0711a5c03d4
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-21 22:56:57 +0100

    Code style & imports fix

--- django_pstch_helpers/models/mixins/edit.py
+++ django_pstch_helpers/models/mixins/edit.py
@@ -1,14 +1,17 @@
+#TODO: Fix module docstring
+from django.core.exceptions import ImproperlyConfigured
+
 from django_pstch_helpers.utils import contribute_viewset_to_views
 
-from django_pstch_helpers.models import AutoPatternsMixin
+from .base import AutoPatternsMixin
 
 from django_pstch_helpers.sets import CreateViewSet, UpdateViewSet
-from django_pstch_helpers.sets.edit.create import SpecificCreateViewSet
+from django_pstch_helpers.sets.edit import SpecificCreateViewSet
 
 class CreatableModelMixin(AutoPatternsMixin):
     def get_create_url(self):
         return self.get_url(CreateViewSet.action,
-                            args = [self.id,])
+                            args=[self.id,])
     def get_views(self):
         views = super(CreatableModelMixin, self).get_views()
         contribute_viewset_to_views(views, CreateViewSet)
@@ -16,23 +19,28 @@ class CreatableModelMixin(AutoPatternsMixin):
 
 class SpecificCreatableModelMixin(CreatableModelMixin):
     def get_views(self):
-        views = super(ListableModelMixin, self).get_views()
+        views = super(SpecificCreatableModelMixin, self).get_views()
         contribute_viewset_to_views(views, SpecificCreateViewSet)
         return views
     def get_views_args(self):
-        args = super(FilteredListableModelMixin, self).get_views_args()
-        args[SpecificCreateViewSet.action] = args.get(SpecificCreateViewSet.action) or {}
-        args[SpecificCreateViewSet.action] = {
+        action = SpecificCreateViewSet.action
+        args = super(SpecificCreatableModelMixin, self).get_views_args()
+        args[action] = args.get() or {}
+        args[action] = {
             'initial_keys' : self.get_specific_create_initial_keys()
         }
         return args
-    def get_specific_create_initial_keys(self):
-        raise ImproperlyConfigured("get_specific_create_initial_keys should be overriden to return a proper list of fields")
+    @staticmethod
+    def get_spec_create_init_keys():
+        raise ImproperlyConfigured(
+            "get_specific_create_initial_keys should be "
+            "overriden to return a proper list of fields"
+        )
 
 class UpdatableModelMixin(AutoPatternsMixin):
     def get_update_url(self):
         return self.get_url(UpdateViewSet.action,
-                            args = [self.id,])
+                            args=[self.id,])
     def get_edit_url(self):
         return self.get_update_url()
     def get_views(self):
