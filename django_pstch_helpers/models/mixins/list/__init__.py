ef50beb1ba0ff045bca60c0b2e4f29db791564aa
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-21 23:22:51 +0100

    Paragraph fills

--- django_pstch_helpers/models/mixins/list/__init__.py
+++ django_pstch_helpers/models/mixins/list/__init__.py
@@ -23,16 +23,22 @@ class ListableModelMixin(AutoPatternsMixin):
         return args
     def get_sort_fields(self):
         """
-        Override this if you want to allow sorting. Should return a dict used as argument to django-sortable-listview (see relevant specification)
+        Override this if you want to allow sorting. Should return a
+        dict used as argument to django-sortable-listview (see
+        relevant specification)
         """
         return []
     def get_paginate_by(self):
         """
-        Override this if you want to use pagination. Should return an integer, that will be used as the value for the 'paginate_by' view keyword argument.
+        Override this if you want to use pagination. Should return an
+        integer, that will be used as the value for the 'paginate_by'
+        view keyword argument.
         """
         return None
     def get_list_select_related_fields(self):
         """
-        Override this to tell Django on which fields it should use select_related (resulting and SQL JOINS). Should return a list of fields (ex: ['category', 'category__phase'])
+        Override this to tell Django on which fields it should use
+        select_related (resulting and SQL JOINS). Should return a list
+        of fields (ex: ['category', 'category__phase'])
         """
         return []
