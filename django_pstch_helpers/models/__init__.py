3925854e3b331e4228b856424e67d5e23ee582d1
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-02-21 08:15:50 +0100

    Code style

--- django_pstch_helpers/models/__init__.py
+++ django_pstch_helpers/models/__init__.py
@@ -7,8 +7,8 @@ from .mixins import ModelInfoMixin
 
 class UserNamed(ModelInfoMixin, models.Model):
     #TODO: Write tests for this class (test at least that __unicode__ is working well) using a sample model
-    name = models.CharField(max_length = 128, verbose_name = "name")
-    slug = AutoSlugField(populate_from = 'name')
+    name = models.CharField(max_length=128, verbose_name="name")
+    slug = AutoSlugField(populate_from='name')
     def __unicode__(self):
         return self.name
     class Meta:
@@ -16,6 +16,6 @@ class UserNamed(ModelInfoMixin, models.Model):
 
 class UserDescribed(UserNamed):
     #TODO: Write tests for this class (test at least that the description can be rendered to HTML) using a sample model
-    description = MarkupField(blank = True, null = True, verbose_name = "description")
+    description = MarkupField(blank=True, null=True, verbose_name="description")
     class Meta:
         abstract = True
