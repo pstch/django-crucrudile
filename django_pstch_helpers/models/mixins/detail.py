a22e1471b59a01ca7646633eaf34aad91b4c3fae
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-20 09:03:49 +0100

    Missing classmethod decorators

--- django_pstch_helpers/models/mixins/detail.py
+++ django_pstch_helpers/models/mixins/detail.py
@@ -9,17 +9,20 @@ class DetailableModelMixin(AutoPatternsMixin):
     """
     #TODO: Add class docstring
     """
+    @classmethod
     def get_detail_url(cls):
         """
         #TODO: Add method docstring
         """
         return cls.get_url(DetailView,
                             args=[cls.id,])
+    @classmethod
     def get_absolute_url(cls):
         """
         #TODO: Add method docstring
         """
         return cls.get_detail_url()
+    @classmethod
     def get_views(cls):
         """
         #TODO: Add method docstring
