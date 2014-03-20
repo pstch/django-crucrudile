6ebceadadf9bc70f95d8cb968b6c96caa4b59809
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-20 08:34:56 +0100

    Add FilteredListableModelMixin test

--- tests/test_model_mixins.py
+++ tests/test_model_mixins.py
@@ -5,16 +5,17 @@ from django.test import TestCase
 from django.db.models import Model
 
 from django_pstch_helpers.models.mixins.list import ListableModelMixin
+from django_pstch_helpers.models.mixins.list.filtered import FilteredListableModelMixin
 
-from django_pstch_helpers.views import ListView
+from django_pstch_helpers.views import ListView, FilteredListView
 
 class ListableModelMixinTestCase(TestCase):
     class TestModel(ListableModelMixin, Model):
         @classmethod
-        def get_sort_fields(cls):
+        def get_list_sort_fields(cls):
             return ['test_sort_field',]
         @classmethod
-        def get_paginate_by(cls):
+        def get_list_paginate_by(cls):
             return 42
         @classmethod
         def get_list_select_related_fields(cls):
@@ -26,6 +27,28 @@ class ListableModelMixinTestCase(TestCase):
 
     def test_get_args_by_view(self):
         self.assertEqual(self.TestModel.get_args_by_view(ListView),
+                         {'allowed_sort_fields' : ['test_sort_field',],
+                          'paginate_by' : 42,
+                          'select_related' : ['test_related_field',]})
+
+class FilteredListableModelMixinTestCase(TestCase):
+    class TestModel(FilteredListableModelMixin, Model):
+        @classmethod
+        def get_filtered_list_sort_fields(cls):
+            return ['test_sort_field',]
+        @classmethod
+        def get_filtered_list_paginate_by(cls):
+            return 42
+        @classmethod
+        def get_filtered_list_select_related_fields(cls):
+            return ['test_related_field',]
+
+    def test_get_views(self):
+        self.assertEqual(self.TestModel.get_views(),
+                         [FilteredListView])
+
+    def test_get_args_by_view(self):
+        self.assertEqual(self.TestModel.get_args_by_view(FilteredListView),
                          {'allowed_sort_fields' : ['test_sort_field',],
                           'paginate_by' : 42,
                           'select_related' : ['test_related_field',]})
