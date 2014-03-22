2960ac8889c4924588a408b044d5fb993a2a20e8
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-22 06:30:48 +0100

    Fix Fix

--- django_pstch_helpers/views/auth.py
+++ django_pstch_helpers/views/auth.py
@@ -11,6 +11,8 @@ from django.views.generic import View
 from django.contrib.auth import login, authenticate, logout
 from django.core.urlresolvers import reverse
 
+from django.utils.translation import ugettext as _
+
 from .mixins.template import TemplateResponseMixin
 
 class LoginView(TemplateResponseMixin, View):
@@ -18,7 +20,7 @@ class LoginView(TemplateResponseMixin, View):
     #TODO: Add class docstring
     """
     fallback_redirect_to = "home"
-    template_name_no_prefix = "auth/login_required.html"
+    template_name = "auth/login_required.html"
 
     def redirect(self, request):
         """
@@ -47,19 +49,23 @@ class LoginView(TemplateResponseMixin, View):
             if user.is_active:
                 login(request, user)
                 # success
-                messages.success(request,
-                                 "Login successful ! User %s connected at %s" \
-                                 % (user.username, datetime.datetime.now()))
+                messages.success(
+                    request,
+                    _("Login successful ! User %s connected at %s") \
+                    % (user.username, datetime.datetime.now())
+                )
                 return self.redirect(request)
             else:
                 # disabled account
-                messages.error(request,
-                               "This account (%s) is disabled." \
-                               % user.username)
+                messages.error(
+                    request,
+                    _("This account (%s) is disabled.") \
+                    % user.username
+                )
                 return self.redirect(request)
         else:
             # invalid login
-            messages.error(request, "Invalid credentials. Please try again.")
+            messages.error(request, _("Invalid credentials. Please try again."))
             return self.redirect(request)
 
 
