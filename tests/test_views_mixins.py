213b20ce111fdb140827ca340af4a4d749980dfa
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-22 05:13:32 +0100

    Cleanup

--- tests/test_views_mixins.py
+++ tests/test_views_mixins.py
@@ -1,40 +1,15 @@
 """
 #TODO: Add module docstring
 """
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.test.client import RequestFactory
-from django.views.generic import View
+from django.views.generic import View as DjangoView
 
 from django.contrib.auth.models import User, AnonymousUser
 
 from django_pstch_helpers.views.mixins.auth import AuthMixin
 
-def setup_view(view, request, *args, **kwargs):
-    """Mimic ``as_view()``, but returns view instance.
-
-    Use this function to get view instances on which you can run unit tests,
-    by testing specific methods.
-
-    This is an early implementation of
-    https://code.djangoproject.com/ticket/20456
-
-    ``view``
-        A view instance, such as ``TemplateView(template_name='dummy.html')``.
-        Initialization arguments are the same you would pass to ``as_view()``.
-
-    ``request``
-        A request object, typically built with
-        :class:`~django.test.client.RequestFactory`.
-
-    ``args`` and ``kwargs``
-        "URLconf" positional and keyword arguments, the same you would pass to
-        :func:`~django.core.urlresolvers.reverse`.
-
-    """
-    view.request = request
-    view.args = args
-    view.kwargs = kwargs
-    return view
+from .utils import setup_view
 
 #pylint: disable=R0201, R0903, R0904, W0232, C0103
 
@@ -42,11 +17,8 @@ class AuthMixinTestCase(TestCase):
     """
     #TODO: Add class docstring
     """
-    class AuthView(AuthMixin, View):
-        """
-        #TODO: Add class docstring
-        """
-        pass
+    view_class = DjangoView # Django view because we only test AuthMixin here
+    mixin_class = AuthMixin
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
@@ -55,9 +27,12 @@ class AuthMixinTestCase(TestCase):
             'test@localhost.localdomain',
             'test'
         )
-        self.view = self.AuthView(required_login=True,
-                                  login_template="test/login.html",
-                                  template_name="test/home.html")
+
+        class AuthView(self.mixin_class, self.view_class): # pylint: disable=C0111
+            pass
+        self.view = AuthView(required_login=True,
+                              login_template="test/login.html",
+                              template_name="test/home.html")
 
     def test_required_login_with_valid_user(self):
         """
