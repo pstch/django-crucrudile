d90d60dbe02ea47f850223bc58c59b1f6028d091
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-22 06:13:15 +0100

    Move ExtraContext test to test_views_mixins, and added TemplateResponse test

--- tests/test_views.py
+++ tests/test_views.py
@@ -23,25 +23,4 @@ class ViewTestCase(TestCase):
     def test_simple_dispatch(self):
         self.assertEqual(type(self.view.dispatch(self.request)),
                          HttpResponseNotAllowed)
-    def test_extra_context(self):
-        # dict extra context
-        test_dict = {'test_key' : 'test_value'}
-        self.view.extra_context = test_dict
-        self.assertEqual(self.view.get_context_data(),
-                         test_dict)
-        # callable extra context
-        test_lambda = lambda v, c: test_dict
-        self.view.extra_context = test_lambda
-        self.assertEqual(self.view.get_context_data(),
-                         test_dict)
-        # dict (with callable values)
-        test_dict_callable_values = {'test_key' : lambda v, c: 'test_value'}
-        self.view.extra_context = test_dict_callable_values
-        self.assertEqual(self.view.get_context_data(),
-                         test_dict)
-        # callable (which retruns a dict with callable values)
-        test_lambda_callable_values = lambda v, c: test_dict_callable_values
-        self.view.extra_context = test_lambda_callable_values
-        self.assertEqual(self.view.get_context_data(),
-                         test_dict)
 
--- tests/test_views_mixins.py
+++ tests/test_views_mixins.py
@@ -8,6 +8,10 @@ from django.views.generic import View as DjangoView
 from django.contrib.auth.models import User, AnonymousUser
 
 from django_pstch_helpers.views.mixins.auth import AuthMixin
+from django_pstch_helpers.views.mixins.context import ExtraContextMixin
+from django_pstch_helpers.views.mixins.template import TemplateResponseMixin
+
+from django_pstch_helpers.views import View
 
 from .utils import setup_view
 
@@ -20,6 +24,9 @@ class AuthMixinTestCase(TestCase):
     view_class = DjangoView # Django view because we only test AuthMixin here
     mixin_class = AuthMixin
     def setUp(self):
+        """
+        #TODO: Add method docstring
+        """
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
         self.user = User.objects.create_user(
@@ -62,3 +69,75 @@ class AuthMixinTestCase(TestCase):
         self.assertTemplateUsed(view.dispatch(request),
                                 "test/login.html")
 
+class ExtraContextTestCase(TestCase):
+    """
+    #TODO: Add class docstring
+    """
+    view_class = DjangoView
+    mixin_class = ExtraContextMixin
+    def setUp(self):
+        """
+        #TODO: Add method docstring
+        """
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.request = self.factory.get('/')
+
+        class ExtraContextView(self.mixin_class, self.view_class): # pylint: disable=C0111
+            pass
+
+        self.view = setup_view(ExtraContextView(),
+                               self.request)
+
+    def test_extra_context(self):
+        """
+        #TODO: Add method docstring
+        """
+        # dict extra context
+        test_dict = {'test_key' : 'test_value'}
+        self.view.extra_context = test_dict
+        self.assertEqual(self.view.get_context_data(),
+                         test_dict)
+        # callable extra context
+        test_lambda = lambda v, c: test_dict
+        self.view.extra_context = test_lambda
+        self.assertEqual(self.view.get_context_data(),
+                         test_dict)
+        # dict (with callable values)
+        test_dict_callable_values = {'test_key' : lambda v, c: 'test_value'}
+        self.view.extra_context = test_dict_callable_values
+        self.assertEqual(self.view.get_context_data(),
+                         test_dict)
+        # callable (which retruns a dict with callable values)
+        test_lambda_callable_values = lambda v, c: test_dict_callable_values
+        self.view.extra_context = test_lambda_callable_values
+        self.assertEqual(self.view.get_context_data(),
+                         test_dict)
+
+class TemplateResponseTestCase(TestCase):
+    """
+    #TODO: Add class docstring
+    """
+    view_class = DjangoView
+    mixin_class = TemplateResponseMixin
+
+    def setUp(self):
+        """
+        #TODO: Add method docstring
+        """
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.request = self.factory.get('/')
+
+        class TemplateResponseView(self.mixin_class, self.view_class): # pylint: disable=C0111
+            pass
+
+        self.view = setup_view(TemplateResponseView(),
+                               self.request)
+
+    def test_get_app_name(self):
+        """
+        #TODO: Add method docstring
+        """
+        self.assertEqual(self.view.get_app_name(),
+                         'tests')
