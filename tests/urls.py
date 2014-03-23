27e63f55edcf3e2d5f0f50cc2413221b6226feda
Hugo Geoffroy <pistache@floyd.pstch.net>
2014-03-23 04:26:30 +0100

    Fix tests URL patterns (fix #9)

--- tests/urls.py
+++ tests/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import patterns, include, url
 
 from django_pstch_helpers.views import TemplateView
 
+test_patterns = patterns(
+    '',
+    url(r'^$',
+        TemplateView.as_view(template_name="test"),
+        name='home'),
+)
+
 urlpatterns = patterns(
     '',
     url(r'^$',
-        TemplateView(template_name="test"),
-        name='home'),
+        include((test_patterns, "test", "test")))
 )
