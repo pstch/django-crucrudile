d53057b08b6c6c9bd358ebdaa8930737947468ce
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-25 07:07:06 +0100

    Add ActionMixin

---
+++ django_pstch_helpers/views/mixins/action.py
@@ -0,0 +1,16 @@
+"""
+#TODO: Add module docstring
+"""
+
+class ActionMixin(object):
+    """
+    #TODO: Add class docstring
+    """
+    def get_action_name():
+        """
+        #TODO: Add method docstring
+        """
+        raise Exception(
+            "The class method get_action_name should "
+            "be overriden when using ActionMixin"
+        )
