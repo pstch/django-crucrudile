27491432fa72e9777f9282d66f3026a965c06271
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-03-25 09:20:27 +0100

    Automatically guess action name from class name
    ex: CreateView becomes create, FilteredListView becomes filtered-list

--- django_pstch_helpers/views/mixins/action.py
+++ django_pstch_helpers/views/mixins/action.py
@@ -8,10 +8,16 @@ class ActionMixin(object):
     """
     @classmethod
     def get_action_name(cls):
+        def get_dashed_name(camel_cased):
+            """
+            #TODO: Add method docstring
+            """
+            step = re.sub('(.)([A-Z][a-z]+)', r'\1-\2', camel_cased)
+            return re.sub('([a-z0-9])([A-Z])', r'\1-\2', step).lower()
+
         """
         #TODO: Add method docstring
         """
-        raise Exception(
-            "The class method get_action_name should "
-            "be overriden when using ActionMixin"
-        )
+        class_name = cls.__name__
+        camel_cased_name = class_name[:-4] if class_name.endswith('View') else class_name
+        return get_dashed_name(camel_cased_name)
