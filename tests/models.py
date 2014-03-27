172cbb5907a3dbbd99ea143888c5b58b748fd583
Hugo Geoffroy <pistache@floyd.pstch.net>
2014-03-27 12:05:05 +0100

    Adapt tests to new version of django-crucrudile

--- tests/models.py
+++ tests/models.py
@@ -1,59 +1,27 @@
 from django.db import models
 from django.db.models import Model
 
-from django_pstch_helpers.models import UserNamed, UserDescribed
-from django_pstch_helpers.models.mixins import (
-    ModelInfoMixin,
-    AutoPatternsMixin,
-    ListableModelMixin,
-    FilteredListableModelMixin,
-    DetailableModelMixin,
-    CreatableModelMixin,
-    SpecificCreatableModelMixin,
-    UpdatableModelMixin,
-    DeletableModelMixin,
-    BaseModelActionsMixin,
-    FullModelActionsMixin,
+from django.generic.views import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
 )
 
-class TestListableModel(ListableModelMixin, Model):
+from django_pstch_helpers.models.mixins import make_model_mixin
+
+class TestListableModel(make_model_mixin(ListView), Model):
     pass
 
-class TestFilteredListableModel(ListableModelMixin, Model):
+class TestDetailableModel(make_model_mixin(DetailView), Model):
     pass
 
-class TestDetailableModel(DetailableModelMixin, Model):
+class TestCreatableModel(make_model_mixin(CreateView), Model):
     pass
 
-class TestCreatableModel(CreatableModelMixin, Model):
+class TestUpdatableModel(make_model_mixin(UpdateView), Model):
     pass
 
-class TestSpecificCreatableModel(SpecificCreatableModelMixin, Model):
+class TestDeletableModel(make_model_mixin(DeleteView), Model):
     pass
-
-class TestUpdatableModel(UpdatableModelMixin, Model):
-    pass
-
-class TestDeletableModel(DeletableModelMixin, Model):
-    pass
-
-class TestBaseModelMixinsModel(BaseModelActionsMixin, Model):
-    pass
-
-class TestFullModelMixinsModel(FullModelActionsMixin, Model):
-    pass
-
-class Genre(BaseModelActionsMixin, UserDescribed):
-    pass
-
-class Author(FullModelActionsMixin, UserDescribed):
-    pass
-
-class Publisher(ListableModelMixin, UserNamed):
-    pass
-
-class Book(FullModelActionsMixin, UserDescribed):
-    isbn = models.CharField(max_length=24)
-    genre = models.ForeignKey(Genre)
-    author = models.ForeignKey(Author)
-    publisher = models.ForeignKey(Publisher)
