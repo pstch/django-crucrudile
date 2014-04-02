56363382bcd8b859ecb80610e915b18996bc2b7a
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-04-02 22:58:20 +0200

    Add test views

---
+++ tests/views.py
@@ -0,0 +1,7 @@
+from django_crucrudile.views.mixins import ModelActionMixin
+
+class AutoPatternsMixinTestView(ModelActionMixin):
+    pass
+
+class AuxAutoPatternsMixinTestView(ModelActionMixin):
+    pass
