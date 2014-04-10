2a8f0db046f177883ebfba570ddcf04509776d02
Hugo Geoffroy <pistache@floyd.pstch.net>
2014-04-10 20:42:02 +0200

    Fixed utils docstrings, removed auto_patterns_for_model, fixed auto_patterns_for_app

--- django_crucrudile/utils.py
+++ django_crucrudile/utils.py
@@ -1,30 +1,62 @@
-"""
-Utility functions
+"""Utility functions
+
+Imports :
+ -- re : needed by convert_camel_case
+ -- django...ImproperlyConfigured : might be raised by auto_patterns_for_app
+ -- itertools.chain : needed by auto_patterns_for_app (to join lists)
+
+Functions :
+ -- auto_patterns_for_app(app_name)
+ -- auto_patterns_for_model(model)
+ -- try_calling(arg, *args, **kwargs)
+ -- convert_camel_case(camel_cased, separator)
+
 """
 import re
+from itertools import chain
 
 from django.core.exceptions import ImproperlyConfigured
 
 def auto_patterns_for_app(app_name):
+    """Returns a list of URL patterns (Django URL objects) for the given
+    application, using content types.
+
+    This function will try to import django.contrib.contenttypes, and
+    fail if it can't.
+    """
     try:
         from django.contrib.contenttypes.models import ContentType
     except ImportError:
-        raise Improperlyconfigured("auto_patterns_for_app must be able to import django.contrib.contenttypes")
-    return ContentType.objects.filter(app_label=app_name)
+        raise ImproperlyConfigured(
+            "auto_patterns_for_app must be able to import"
+            " django.contrib.contenttypes"
+        )
+    content_types = ContentType.objects.filter(app_name=app_name)
+    return list(chain(
+        *[c.model_class().get_url_patterns() for c in content_types]
+    ))
 
-def auto_patterns(content_types):
-    patterns = []
-    for content_type in content_types:
-        patterns += content_type.model_class().get_url_patterns()
-    return patterns
+def auto_patterns_for_model(model):
+    """Returns a list of URL patterns (Django URL objects) for the given
+    Model class
+    """
+    return model.get_url_patterns()
 
-def call_if_needed(arg, *args, **kwargs):
-    return arg if not callable(arg) else arg(*args, **kwargs)
+
+def try_calling(arg, *args, **kwargs):
+    """Evaluate and return arg if it's a callable, otherwise return None
+    """
+    return arg(*args, **kwargs) if callable(arg) else None
 
 
 def convert_camel_case(camel_cased, separator):
-    """
-    #TODO: Add method docstring
+    """Convert camel cased into words separated by the given separator
+
+    Keywords :
+    -- camel_cased (ex:"CamelCased")
+    -- separator (ex:"-")
+
+    Above parameters will return "camel-cased"
     """
     separator_expression = r'\1%s\2' % separator
     step = re.sub('(.)([A-Z][a-z]+)',
