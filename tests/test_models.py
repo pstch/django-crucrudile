638af3e00dea47369d6f53a0fff14c5fd2d2995d
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-04-13 10:17:24 +0200

    Add testmodel testcase

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from .models import TestModel
 
 class TestModelTestCase(TestCase):
+    url_func_name = 'get_test_action_url'
     def setUp(self):
         self.model_class = TestModel
 
@@ -10,6 +11,26 @@ class TestModelTestCase(TestCase):
         self.assertTrue(
             hasattr(
                 self.model_class,
-                'get_test_action_url'
+                self.url_func_name
             )
+        )
+    def test_url_func_callable(self):
+        self.assertTrue(
+            callable(
+                getattr(
+                    self.model_class,
+                    self.url_func_name,
+                    None
+                )
+            )
+        )
+    def test_url_func(self):
+        url_func = getattr(
+            self.model_class,
+            self.url_func_name,
+            lambda *args, **kwargs: None
+        )
+        self.assertEqual(
+            url_func(),
+            '/testmodel/test-action'
         )
