726fd172692053364d8db67f48ec293752de7910
Hugo Geoffroy <pistache@floyd.pstch.net>
2014-04-15 12:23:56 +0200

    Fix imports in test

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 
-from django_crucrudile.utils import auto_patterns_for_app
+from django_crucrudile.urls import auto_patterns_for_app
 
 x = lambda x: None
 
