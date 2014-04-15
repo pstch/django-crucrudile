12623fca239d94679828613c65ed1faaed5cf5fc
Hugo Geoffroy <pistache@floyd.pstch.net>
2014-04-15 12:54:41 +0200

    Add module docstrings

--- django_crucrudile/__init__.py
+++ django_crucrudile/__init__.py
@@ -1,3 +1,13 @@
-"""
-#TODO: Add module docstring
+"""django-crucrudile allows you to create "model mixins", that define
+possible actions for this model. Those model mixins allow the model to
+be able to generate its URL patterns by itself, so that they can be
+included in urls.py using just a call to the get_url_patterns() method
+of the model class.
+
+Modules :
+-- models.mixins : model mixins and functions
+-- views.mixins : view mixins
+-- urls : automatic URL patterns functions
+-- utils : utility functions
+
 """
---
+++ django_crucrudile/views/__init__.py
@@ -0,0 +1,5 @@
+"""View-related module, contains the mixins module which provides
+ModelActionMixin, needed to store the metadata required to create URL
+patterns for a model-view combination
+
+"""
