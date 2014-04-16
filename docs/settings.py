58f4ee3ba1d9e09378194fc9f46d1dbfbe422b48
Hugo Geoffroy <github@pstch.net>
2014-04-16 20:45:38 +0200

    Update settings.py

--- docs/settings.py
+++ docs/settings.py
@@ -6,9 +6,7 @@ DATABASES = {
 }
 INSTALLED_APPS = (
     'django.contrib.contenttypes',
-    'django.contrib.auth',
     'django_crucrudile',
-    'tests',
 )
 
 MARKITUP_FILTER = ('markdown.markdown', {'safe_mode': True})
