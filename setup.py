55eb40d973d47166eba856377f13538a7a9e06d7
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-04-16 19:06:35 +0200

    yallah

--- setup.py
+++ setup.py
@@ -10,17 +10,26 @@ from setuptools import setup, find_packages
 setup(
     name='django-crucrudile',
     version='0.3',
+
     description='Model-defined CRUD views & patterns for Django',
     long_description="views, models, auto URL patterns, ...",
+
     url='https://github.com/pstch/django-crucrudile',
+
     author='Hugo Geoffroy',
     author_email='hugo@pstch.net',
+
     packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'Django == 1.6'
+    ],
+
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     platforms=['any'],
+
     test_suite='runtests.runtests',
 )
