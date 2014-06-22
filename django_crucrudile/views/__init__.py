c96d7c6c755da277dd8c830e2e78ad1f7e3b514b
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-06-22 22:25:48 +0200

    New routers branch : delete old code & docs

--- django_crucrudile/views/__init__.py
+++
@@ -1,5 +0,0 @@
-"""View-related module, contains the mixins module which provides
-ModelActionMixin, needed to store the metadata required to create URL
-patterns for a model-view combination
-
-"""
--- django_crucrudile/viewsets/action.py
+++
@@ -1,2 +0,0 @@
-def provides(viewset, view_class, view_args):
-    viewset.views[view_class].update(view_args)
