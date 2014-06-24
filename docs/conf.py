93a6d5e3a046b33ec89748cefe20a503b39da272
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-06-24 08:29:01 +0200

    Fix settings module in sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,8 @@ sys.path.insert(
     )
 )
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
