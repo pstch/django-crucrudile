9a6963d9b883b10bbda7e9bb98351155e5ba62fb
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-06-25 06:04:16 +0200

    Add viewcode Sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.inheritance_diagram',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
