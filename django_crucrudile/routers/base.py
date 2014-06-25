0a3f17a44ab1fd59ba0f15585115466e69427505
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-06-25 06:53:42 +0200

    Update class structure

--- django_crucrudile/routers/base.py
+++ django_crucrudile/routers/base.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
 
+from django.conf.urls import url, include
+
 
 class RoutedEntity(metaclass=ABCMeta):
     """Abstract class for routed entities
@@ -91,24 +93,6 @@ class RoutedEntity(metaclass=ABCMeta):
         return '\n'.join(
             pattern_lines
         )
-
-
-class BaseRoute(RoutedEntity):
-    pass
-
-
-class BaseModelRoute(BaseRoute):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-
-    @property
-    def model_url_part(self):
-        return self.model._meta.model_name
-
-    @property
-    def model_url_name(self):
-        return self.model._meta.model_name
 
 
 class BaseRouterMetaclass(ABCMeta):
@@ -258,16 +242,102 @@ class BaseRouter(RoutedEntity, metaclass=BaseRouterMetaclass):
         if index or entity.index:
             self.redirect = entity
 
+class BaseRoute(RoutedEntity):
+    name = None
+    url_part = None
+    auto_url_part = True
+
+    def __init__(self, name=None, url_part=None):
+        if name is not None:
+            self.name = name
+        elif self.name is None:
+            raise Exception(
+                "No ``name`` argument provided to __init__"
+                ", and no name defined as class attribute."
+                " (in {})".format(self)
+            )
+        if url_part is not None:
+            self.url_part = url_part
+        elif self.url_part is None:
+            if self.auto_url_part:
+                self.url_part = self.name
+            else:
+                raise Exception(
+                    "No ``url_part`` argument provided to __init__"
+                    ", and no url_part defined as class attribute."
+                    " (in {})".format(self)
+                )
+
+    def patterns(self, *args, **kwargs):
+        callback = self.get_callback()
+        url_name = self.get_url_name()
+        for url_part in self.get_url_parts():
+            yield url(
+                url_part,
+                callback,
+                url_name
+            )
+
+    @abstractmethod
+    def get_callback(self):
+        pass
+
+    def get_url_parts(self):
+        yield self.url_part
+
+    def get_url_name(self):
+        return self.name
+
+class BaseViewRoute(BaseRoute):
+    def __init__(self, view_class=None, name=None):
+        super().__init__(name)
+        if view_class is not None:
+            self.view_class = view_class
+        elif self.view_class is None:
+            raise Exception(
+                "No ``view_class`` argument provided to __init__"
+                ", and no view_class defined as class attribute (in {})"
+                "".format(self)
+            )
+
+class BaseModelRoute(BaseRoute):
+    def __init__(self, model=None,
+                 view_class=None, name=None):
+        if model is not None:
+            self.model = model
+        elif self.model is None:
+            raise Exception(
+                "No ``model`` argument provided to __init__"
+                ", and no model defined as class attribute (in {})"
+                "".format(self)
+            )
+        super().__init__(view_class, name)
+
 
 class BaseModelRouter(BaseRouter):
     """.. inheritance-diagram:: BaseModelRouter
     """
-
-    def __init__(self, model):
-        self.model = model
-        super().__init__()
-
+    model = None
     def get_auto_register_kwargs(self):
+        """Add ModelRouter model to upstream auto register kwargs, so that the
+        route classes in the base store will get the model as a kwarg when
+        being instantiated.
+
+        """
         kwargs = super().get_auto_register_kwargs()
         kwargs['model'] = self.model
         return kwargs
+
+    def __init__(self, model=None):
+        # we need to set self.model before calling the superclass
+        # __init__, because it will call
+        # self.get_auto_register_kwargs() which needs self.model
+        if model is not None:
+            self.model = model
+        elif self.model is None:
+            raise Exception(
+                "No ``model`` argument provided to __init__"
+                ", and no model defined as class attribute (in {})"
+                "".format(self)
+            )
+        super().__init__()
