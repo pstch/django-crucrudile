18ef3ac23ab5146756d0f690f084b482899dc1e5
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-06-28 08:03:04 +0200

    Simplify example app urls.Py

--- tests/example_app/example_app/urls.py
+++ tests/example_app/example_app/urls.py
@@ -1,13 +1,1 @@
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-from .routers import base_router
-
-urlpatterns = patterns(
-    '',
-    url(r'^', include(
-        list(base_router.patterns())
-    ))
-)
+urlpatterns = list(__import__('.routers').base_router.patterns())
