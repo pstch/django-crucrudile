a6d5944451c6f9b66f186bb7fb94a12bb82cfbf8
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-06-30 13:33:22 +0200

    Fix sphinx mistake

--- django_crucrudile/entities/store.py
+++ django_crucrudile/entities/store.py
@@ -55,7 +55,7 @@ def provides(provided, **kwargs):
 
 def register_instances(to_store):
     """Return a decorator that makes all instances of this class (and its
-    subclasses) automatically register themselves to :argument:`to_store`.
+    subclasses) automatically register themselves to ``to_store``.
 
     :argument to_store: Store to register instances to.
     :type to_store: :class:`EntityStore`
