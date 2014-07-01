705bdf392ee7edb1755182f4e39c4b8ad5c42754
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-07-01 11:55:06 +0200

    Make runtests.py run sphinx doctests

--- runtests.py
+++ runtests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#pylint: disable=W0142,C0111
+# pylint: disable=W0142,C0111
 """
 #TODO: Missing module docstring
 """
@@ -8,13 +8,26 @@ import os
 
 from django.core.management import execute_from_command_line
 
-def runtests():
+
+def run_django_tests():
+    print("#### Running tests using Django test runner...")
     argv = ['runtests.py', 'test'] + sys.argv[1:]
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     execute_from_command_line(argv)
-    sys.exit(0)
+    print("#### Done running tests with Django test runner.")
 
+
+def run_sphinx_tests():
+    print("#### Running tests using Sphinx doctest builder...")
+    print("#### (Disable with NO_SPHINX_TESTS=1)")
+    os.system("sphinx-build -E -c docs -b doctest -a docs doctests")
+    print("#### Done running tests using Sphinx doctest builder.")
+
+
+def runtests():
+    run_django_tests()
+    if os.environ.get('NO_SPHINX_TESTS') != '1':
+        run_sphinx_tests()
 
 if __name__ == '__main__':
     runtests()
-
