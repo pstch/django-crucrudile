36c22efd4480a98470fddcc40c0caa42f46debd0
Hugo Geoffroy <pistache@tranquility.pstch.net>
2014-07-03 05:40:34 +0200

    New crazy runtests script

--- runtests.py
+++ runtests.py
@@ -3,33 +3,75 @@
 """
 #TODO: Missing module docstring
 """
-import sys
 import os
+import sys
+import nose
 
 
-def run_django_tests():
-    print("#### Running tests using Django test runner...")
-    print("#### (Disable with NO_DJANGO_TESTS=1)")
-    argv = ['runtests.py', 'test'] + sys.argv[1:]
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(argv)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
+
+
+def run_nose_main():
+    print("#### Running tests using nose test runner...")
+    print("#### (Disable with {}=1)".format(run_nose_main.disable_with))
+    return nose.main()
     print("#### Done running tests with Django test runner.")
 
 
-def run_sphinx_tests():
+def run_sphinx_main():
     print("#### Running tests using Sphinx doctest builder...")
-    print("#### (Disable with NO_SPHINX_TESTS=1)")
+    print("#### (Disable with {}=1)".format(run_sphinx_main.disable_with))
     import sphinx
-    sphinx.main(['-E', '-b', 'doctest', 'docs', 'doctests'])
+    return sphinx.main(['-E', '-b', 'doctest', 'docs', 'var/docs_doctests'])
     print("#### Done running tests using Sphinx doctest builder.")
+
+run_nose_main.disable_with = "NO_NOSE_TESTS"
+run_sphinx_main.disable_with = "NO_SPHINX_TESTS"
+
+
+def fails(runner, *args, **kwargs):
+    try:
+        return bool(runner(*args, **kwargs))
+    except SystemExit as ex:
+        return ex.code
+    except Exception as ex:
+        print("## {} failed with : {}".format(runner, ex))
+        return True
+    return False
+
+
+def safely_run_if_enabled(runner):
+    if os.environ.get(runner.disable_with) != '1':
+        return not fails(runner)
+
+
+def try_tests(tests):
+    for test in tests:
+        yield safely_run_if_enabled(test)
+
+
+TESTS = [
+    run_nose_main,
+    run_sphinx_main
+]
 
 
 def runtests():
-    if os.environ.get('NO_DJANGO_TESTS') != '1':
-        run_django_tests()
-    if os.environ.get('NO_SPHINX_TESTS') != '1':
-        run_sphinx_tests()
+    print("## django-crucrudile test runner script")
+    results = list(try_tests(TESTS))
+    print("## Test results : {}".format(
+        dict(
+            zip(
+                (test.__name__ for test in TESTS),
+                results)
+        )
+    ))
+    if not all(results):
+        print("## Error: (at least) a test runner failed !")
+        sys.exit(1)
+    else:
+        print("## OK")
 
 if __name__ == '__main__':
+    print("## django-crucrudile test runner script")
     runtests()
