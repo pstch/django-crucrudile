3cca2850f266fd8e96f91f8d0d29cced1ea68c4d
Hugo Geoffroy <github@pstch.net>
2014-07-30 05:09:55 +0200

    Add description and longdescription to package metadata

--- django_crucrudile/__init__.py
+++ django_crucrudile/__init__.py
@@ -25,6 +25,11 @@ Starting from version ``0.9.5``, this package uses semantic version
 """
 __title__ = 'django-crucrudile'
 __description__ = 'Django URL routing classes',
+__long_description = """
+django-crucrudile provides URL routing classes, which allows you 
+to define your URL routing structure using Router and Route classes, 
+and then to automatically generate an URL pattern structure.
+"""
 
 __version__ = '1.0.0-rc1'
 
